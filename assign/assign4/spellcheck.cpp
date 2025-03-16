d8d575248f9290bfce17a8fe844f3a045a7371f7
Gktwerk <gkwangbinqi@gmail.com>
2025-03-16 15:28:19 +0800

    update

--- assign/assign4/spellcheck.cpp
+++ assign/assign4/spellcheck.cpp
@@ -10,19 +10,24 @@
 template <typename Iterator, typename UnaryPred>
 std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 
+// Corpus tokenize(const std::string& source) {return Corpus();}
+
 Corpus tokenize(const std::string& source) {
   /* TODO: Implement this method */
-  auto iterators = find_all(source.begin(), source.end(), ::isspace);
-  std::set<Token> tokens;
+  Corpus tokens;
+
+  auto iterators = find_all(source.begin(), source.end(), isspace);
+  // 这个函数看一下cppref的实现例子更好理解
   std::transform(iterators.begin(), iterators.end(), iterators.begin()+1
   , std::inserter(tokens, tokens.end())
   , [&source](auto it1, auto it2){
     return Token{source, it1, it2};
   });
-  std::erase_if(tokens, [](Token t){
-    if (t.content.empty()) return true;
-    return false;
+
+  std::erase_if(tokens, [](const auto& token){
+    return token.content.empty();
   });
+
   return tokens;
 }
 
